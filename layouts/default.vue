704eba28c35fb9958d00a7d777f0ebace01b7439
Kento Tani <k.tani0212@gmail.com>
2023-06-08 13:04:44 +0900

    init

---
+++ layouts/default.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-app>
+    <v-app-bar
+      app
+      fixed
+      flat
+      dense
+      color="white"
+    >
+      <v-toolbar-title>{{ title }}</v-toolbar-title>
+    </v-app-bar>
+
+    <v-main>
+      <nuxt />
+    </v-main>
+
+    <v-footer
+      app
+      absolute
+      color="white"
+    >
+      <span>&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'DefaultLayout',
+  data () {
+    return {
+      title: 'Nuxt Sandbox'
+    }
+  }
+}
+</script>
+
+<style>
+body::-webkit-scrollbar {
+  display: none;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-container>
+    <div>aaa</div>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
